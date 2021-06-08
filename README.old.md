13d82a8c6f7a4b25fab70409a14a1f0616e933d5
Kubi Kubisiak <mckubisiak@gmail.com>
2021-06-08 13:57:07 -0700

    intial files needed and templates down

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# A-GALLERY-LAB-02
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from './Header';
+import ImageList from './ImageList';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <ImageList />
+    </div>
+  );
+}
+
+export default App;
