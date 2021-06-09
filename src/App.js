81436681a5ad6246673769326d1be23bb4cf6665
Kubi Kubisiak <mckubisiak@gmail.com>
2021-06-09 15:21:57 -0700

    added horns drop down and sorting function

--- src/App.js
+++ src/App.js
@@ -2,41 +2,48 @@ import Header from './Header';
 import ImageList from './ImageList';
 import './App.css';
 import images from './data.js';
-
-
 import React, { Component } from 'react'
 import Dropdown from './Dropdown';
 
 export default class App extends Component {
   state = {
     filterKeyword: '',
-    // filterHorns: ''
-    
+    filterHorns: ''
 }
 
 handleKeywordChange = (e) => {
     this.setState({ filterKeyword: e.target.value })
 }
-// handleHornsChange = (e) => {
-//     this.setState({ filterHorns: e.target.value })
-// }
+handleHornsChange = (e) => {
+    this.setState({ filterHorns: e.target.value })
+}
+
   render() {
     let filteredKeyword = images;
 
-    
     if (this.state.filterKeyword) {
-      filteredKeyword = images.filter(creature => creature.keyword === this.state.filterKeyword) 
+      filteredKeyword = filteredKeyword.filter(creature => creature.keyword === this.state.filterKeyword) 
     }
-    
+
+    if (this.state.filterHorns) {
+      filteredKeyword = filteredKeyword.filter(creature => creature.horns === +this.state.filterHorns) 
+    }
+
     const filteredCreature = Array.from(new Set(images.map(creature =>creature.keyword)))
-    console.log(filteredCreature);
+    const filteredHorns = Array.from(new Set(images.map(creature =>creature.horns)))
+    console.log(this.state.filterHorns);
     return (
       <div>
         
           <Header />
+          Creature type:
           <Dropdown 
           handleChange={this.handleKeywordChange} 
           options={filteredCreature} />
+          Horns:
+          <Dropdown 
+          handleChange={this.handleHornsChange} 
+          options={filteredHorns} />
 
 
       <ImageList data={filteredKeyword} />
@@ -44,7 +51,3 @@ handleKeywordChange = (e) => {
     )
   }
 }
-
-
-
-
--- src/Dropdown.js
+++ src/Dropdown.js
@@ -5,12 +5,12 @@ export default class Dropdown extends Component {
         return (
             <div>
                 
-                <label>  Number of Keyword
+                <label>   
                     <select onChange={this.props.handleChange}>
                     
                             <option value=""> All </option>
-                            {this.props.options.map(selection => 
-                            <option value={selection}> {selection} </option>
+                            {this.props.options.map((selection, i) => 
+                            <option key={i} value={selection}> {selection} </option>
                             )}
                     
                     </select>
@@ -19,18 +19,3 @@ export default class Dropdown extends Component {
         )
     }
 }
-
-
-
-                            /* <option value="narwhal"> narwhal </option>
-                            <option value="rhino"> rhino </option>
-                            <option value= "unicorn"> unicorn </option>
-                            <option value= "unilego"> unilego </option>
-                            <option value= "triceratops"> triceratops </option>
-                            <option value= "markhor"> markhor </option>
-                            <option value= "mouflon"> mouflon </option>
-                            <option value= "addax"> addax </option>
-                            <option value= "chameleon"> chameleon </option>
-                            <option value= "lizard"> lizard </option>
-                            <option value= "dragon"> dragon </option> 
-                             */
